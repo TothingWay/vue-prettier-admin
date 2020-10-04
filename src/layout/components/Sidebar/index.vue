607f58835cbdfcb7a0a483d46fdd600fa0b9ebe0
tothingway <200846290@qq.com>
2020-10-04 17:27:07 +0800

    feat: add layout

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    1
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
