b2ad0446ab49f79ecbed1a6af5cca18d3aeaf7e3
tothingway <200846290@qq.com>
2020-10-04 17:22:59 +0800

    style: code format

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -1,23 +1,23 @@
 <template>
-  <SvgIcon :type="type"/>
+  <SvgIcon :type="type" />
 </template>
 
 <script>
-import { createFromIconfontCN } from '@ant-design/icons-vue';
+import { createFromIconfontCN } from '@ant-design/icons-vue'
 const SvgIcon = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_1755020_ecxcxu2bte.js', // 在 iconfont.cn 上生成
-});
+  scriptUrl: '//at.alicdn.com/t/font_1755020_ecxcxu2bte.js' // 在 iconfont.cn 上生成
+})
 
 export default {
   components: {
-    SvgIcon,
+    SvgIcon
   },
   props: {
     type: {
       type: String,
       default: null
-    },
-  },
+    }
+  }
 }
 </script>
 
