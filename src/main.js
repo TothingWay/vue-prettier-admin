e9443d757f0c43ac320b3260ae2dc521b345f38c
tothingway <200846290@qq.com>
2020-10-06 14:06:59 +0800

    feat: format code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import antd from 'ant-design-vue'
-import router from "./router";
-import store from "./store";
+import router from './router'
+import store from './store'
 import 'ant-design-vue/dist/antd.css'
 import 'normalize.css/normalize.css'
 import Icon from '/@/components/Icon/index.vue'
