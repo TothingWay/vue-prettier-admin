811a019e33c3ec0fc596efa12a68601cd02d88f3
tothingway <200846290@qq.com>
2020-10-06 14:05:49 +0800

    feat: add store modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,20 @@
 import { createStore } from 'vuex'
 import getters from './getters'
 import app from './modules/app'
+import permission from './modules/permission'
+import tagsView from './modules/tagsView'
+import user from './modules/user'
+import errorLog from './modules/errorLog'
+import settings from './modules/settings'
 
 export default createStore({
   getters,
   modules: {
-    app
+    app,
+    permission,
+    tagsView,
+    user,
+    errorLog,
+    settings
   }
 })
