1ece07913825a25e203a6afbf3acf3cc10872d3c
tothingway <200846290@qq.com>
2020-10-06 14:05:14 +0800

    feat: add router config

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,47 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '/@/views/Home.vue'
 
-const routes = [
+/* Layout */
+import Layout from '/@/layout/index.vue'
+
+/**
+ * Note: sub-menu only appear when route children.length >= 1
+ * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
+ *
+ * hidden: true                   if set true, item will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu
+ *                                if not set alwaysShow, when item has more than one children route,
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    roles: ['admin','editor']    control the page roles (you can set multiple roles)
+    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
+    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
+    noCache: true                if set true, the page will no be cached(default is false)
+    affix: true                  if set true, the tag will affix in the tags-view
+    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
+    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
+  }
+ */
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+export const constantRoutes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('/@/views/Home.vue'),
+        name: 'Dashboard',
+        meta: { title: 'Dashboard', icon: 'icon-xiai' }
+      }
+    ]
   }
   // {
   //   path: "/about",
@@ -15,9 +51,16 @@ const routes = [
   // }
 ]
 
+/**
+ * asyncRoutes
+ * the routes that need to be dynamically loaded based on user roles
+ */
+export const asyncRoutes = []
+
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  scrollBehavior: () => ({ top: 0 }),
+  routes: constantRoutes
 })
 
 export default router
