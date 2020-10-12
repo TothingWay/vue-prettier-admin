a6cf785570d6a9e4f5712811d06dda6865f4241d
tothingway <200846290@qq.com>
2020-10-12 14:29:26 +0800

    style: change navbar style

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <a-layout-header :style="{ background: '#fff', padding: 0 }">
-    <Hamburger :is-active="sidebar.opened" @toggle-click="toggleSideBar" />
+  <a-layout-header>
+    <Hamburger class="hamburger-container" :is-active="sidebar.opened" @toggle-click="toggleSideBar" />
   </a-layout-header>
 </template>
 
@@ -37,5 +37,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.ant-layout-header {
+  padding: 0;
+  height: 50px;
+  line-height: 50px;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0,21,41,.08);
+  .hamburger-container {
+    line-height: 46px;
+    height: 100%;
+    float: left;
+    cursor: pointer;
+    transition: background .3s;
+    -webkit-tap-highlight-color:transparent;
 
+    &:hover {
+      background: rgba(0, 0, 0, .025)
+    }
+  }
+}
 </style>
